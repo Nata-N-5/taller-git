9d30a90636469f56a3f79d71c5f8626c9bb99409
Natalia Noguera <nnogueraf@academia.usbbog.edu.co>
2025-08-11 17:49:13 -0500

    nodejs

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 console.log("Jueguito Adivina el número (del 1 al 100)");
 const numeroSecreto = Math.floor(Math.random() * 100) + 1; // Número del 1 al 100
 
+
+const readline = require('readline'); //modulo para leer y escribir en la consola
+
+const rl = readline.createInterface({
+  input: process.stdin, //indica que lee del teclado 
+  output: process.stdout //indica que escribe en la consola
+});
+
 function preguntar(){
 
 }
