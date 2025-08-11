63bdecba661cb0c401a62db17e2bec964a9c8c7c
Natalia Noguera <nnogueraf@academia.usbbog.edu.co>
2025-08-11 17:52:53 -0500

    validar la respuestaxd

--- game.js
+++ game.js
@@ -14,6 +14,12 @@ function preguntar(){//se crea la funcion para evaluar
 
      const numero = parseInt(respuesta);//convertir a numero entero para que funcione :v 
 
+    // Validar si lo que escribió No es un número o está fuera del rango
+    if (isNaN(numero) || numero < 1 || numero > 100) { //NOTA:isNaN Comprueba  el valor 
+      console.log(" Entrada inválida. Por favor, ingresa un número entre 1 y 100.");
+      return preguntar(); // volver a preguntar
+    }
+
     //evaluar la respuesta 
     if (numero === numeroSecreto) {
       console.log("Correcto");
